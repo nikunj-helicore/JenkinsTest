fb34354eb31bc745d4633e6e9a39135e209fa6c8
nikunj-helicore <nikunj.helicore@gmail.com>
2024-08-12 10:52:00 +0530

    Test Commit

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from jenkins");
         System.out.println("First commit");
+        System.out.println("Build executed when code push in master branch");
     }
 }
 
