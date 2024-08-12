cfc2bef8b71b622c20f6b37373c315fcd714b983
nikunj-helicore <nikunj.helicore@gmail.com>
2024-08-12 12:29:25 +0530

    Test Commit

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Code Change");
+        Student student1 = new Student();
+        student1.sayHello();
     }
 }
 
---
+++ Student.java
@@ -0,0 +1,9 @@
+public class Student {
+    public Student(){
+        System.out.println("Creating a Student");
+    }
+
+    public void sayHello(){
+        System.out.println("Hello from Student");
+    }
+}
